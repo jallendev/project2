bb106b586511f85f55d1e72ec8dac415b16e976b
Joseph Allen <developer.joseph.allen@gmail.com>
2019-09-29 17:59:59 -0500

    Completed LetterAvg

--- src/LetterAvg.java
+++ src/LetterAvg.java
@@ -1,10 +1,29 @@
+import java.io.IOException;
 
 public class LetterAvg {
-	public LetterAvg(char avg) {
-		
+	private char letter = 0;
+	
+	public LetterAvg(char letter) {
+		this.letter = letter
 	}
 	
-	public int numberOfStationWithLetterAvg() {
-		return 0;
+	public int numberOfStationWithLetterAvg() throws IOException {
+		return new MesoInherit(new MesoStation("NRMN")).getNoStationsForLetter(letter);
+	}
+	
+	@Override
+	public String toString() {
+		String output = "They are:\n";
+		String[] stations = new String[0];
+		try {
+			stations = new MesoInherit(new MesoStation("NRMN")).getStationsForLetter(letter);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for (int index = 0; index < stations.length; ++index) {
+			output += stations[index] + "\n";
+		}
+		return output;
 	}
 }
