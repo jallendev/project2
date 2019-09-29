72f771dc3a3aa8b9da78b30673c98cc1398a59ff
Joseph Allen <developer.joseph.allen@gmail.com>
2019-09-29 17:13:32 -0500

    Completed PosAvg

--- src/PosAvg.java
+++ src/PosAvg.java
@@ -1,17 +1,26 @@
+import java.io.IOException;
 
 public class PosAvg {
-	public PosAvg(String stId) {
-		
+	private String stID = "";
+	
+	private int index = 0;
+	
+	private String[] avgStations = new String[4];
+	
+	public PosAvg(String stID) throws IOException {
+		MesoInherit temp = new MesoInherit(new MesoStation(stID));
+		this.stID = stID;
+		this.index = temp.getIndex();
+		this.avgStations = temp.getAvgStations(this.index);
 	}
 	
 	public int indexOfStation() {
-		return 0;
+		return this.index;
 		
 	}
 	
 	@Override
 	public String toString() {
-		return null;
-		
+		return "This index is average of " + avgStations[0] + " and " + avgStations[1] + ", " + avgStations[2] + " and " + avgStations[3] + ", and so on.";
 	}
 }
